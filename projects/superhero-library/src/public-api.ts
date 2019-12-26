7adc5f15a04fae577001ceec704b0e5907f22e80
Akash Gupta <akash@adias.tech>
2019-12-26 10:55:25 +0530

    Revert "banner added"
    This reverts commit 591d17a6e2e3f605c7cad2374fa0ed92dba26156.

--- projects/superhero-library/src/public-api.ts
+++
@@ -1,7 +0,0 @@
-/*
- * Public API Surface of superhero-library
- */
-
-export * from './lib/superhero-library.service';
-export * from './lib/superhero-library.component';
-export * from './lib/superhero-library.module';
